82aca1d3b2ef7aaf25f812ac7845934fcc996af3
Félix Perron-Brault <fpbrault@gmail.com>
2021-05-08 18:58:27 +0000

    Fixed prisma issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,23 @@
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from "@prisma/client";
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+import Adapters from "next-auth/adapters";
 
-let prisma: PrismaClient;
+const prismaClientPropertyName = `__prevent-name-collision__prisma`;
+type GlobalThisWithPrismaClient = typeof globalThis & {
+	[prismaClientPropertyName]: PrismaClient;
+};
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
-export default prisma;
+const getPrismaClient = () => {
+	if (process.env.NODE_ENV === `production`) {
+		return new PrismaClient();
+	} else {
+		const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
+		if (!newGlobalThis[prismaClientPropertyName]) {
+			newGlobalThis[prismaClientPropertyName] = new PrismaClient();
+		}
+		return newGlobalThis[prismaClientPropertyName];
+	}
+};
+const prisma= getPrismaClient();
+
+export default Adapters.Prisma.Adapter({prisma});
