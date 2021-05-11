a0cd336dd4614ac1fde119dcf1b320c3e0ec1d88
Félix Perron-Brault <fpbrault@gmail.com>
2021-05-11 19:27:34 -0400

    feat: 🎸 added some custom height and padding classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            height: {
+                '75vh': '75vh',
+                '5vh': '5vh',
+                '20vh': '20vh'
+            },
+            spacing: {
+                '5vh': '5vh'
+            }
+        }
     },
     variants: {
         extend: {}
